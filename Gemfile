5278904a63f958cd1251bb71a8321a60a8bb2ebc
mihail <varstt@ya.ru>
2019-03-22 01:49:18 +0500

    - Add file public/assets

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 gem 'rails', '~> 5.2.2'
 gem 'pg'
-gem 'puma', '~> 3.12'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'bootstrap', '~> 4.2', '>= 4.2.1'
@@ -23,6 +22,10 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
+group :production do
+  gem 'unicorn', '~> 4.8.3'
+end
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
@@ -33,6 +36,11 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'letter_opener', '~> 1.7'
+  gem 'puma', '~> 3.12'
+  gem 'capistrano', '~> 3.11'
+  gem 'capistrano-rvm', '~> 0.1.2'
+  gem 'capistrano-bundler', '~> 1.5'
+  gem 'capistrano-rails', '~> 1.4'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/deploy.rb
@@ -0,0 +1,110 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "pmtools"
+# Репозиторий
+set :repo_url, 'git@github.com:MihailHiminec/pmtools.git'
+# Используемое окружение
+set :rails_env, 'production'
+# Имя пользователя
+set :username, 'deploy'
+# Путь для деплоя
+set :deploy_to, "/home/#{fetch(:username)}/#{fetch(:application)}"
+
+set :log_level, :info
+
+set :linked_dirs, %w{public/upload}
+
+set :linked_files, %w{config/credentials.yml.enc config/database.yml}
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+namespace :setup do
+  desc 'Загрузка конфигурационных файлов на удаленный сервер'
+  task :upload_config do
+    on roles :all do
+      execute :mkdir, "-p #{shared_path}"
+      ['shared/config', 'shared/run'].each do |f|
+        upload!(f, shared_path, recursive: true)
+      end
+    end
+  end
+end
+
+namespace :nginx do
+  desc 'Создание симлинка в /etc/nginx/conf.d на nginx.conf приложения'
+  task :append_config do
+    on roles :all do
+      sudo :ln, "-fs #{shared_path}/config/nginx.conf /etc/nginx/conf.d/#{fetch(:application)}.conf"
+    end
+  end
+  desc 'Релоад nginx'
+  task :reload do
+    on roles :all do
+      sudo :service, :nginx, :reload
+    end
+  end
+  desc 'Рестарт nginx'
+  task :restart do
+    on roles :all do
+      sudo :service, :nginx, :restart
+    end
+  end
+  after :append_config, :restart
+end
+
+set :unicorn_config, "#{shared_path}/config/unicorn.rb"
+set :unicorn_pid, "#{shared_path}/run/unicorn.pid"
+
+namespace :application do
+  desc 'Запуск Unicorn'
+  task :start do
+    on roles(:app) do
+      execute "cd #{release_path} && ~/.rvm/bin/rvm default do bundle exec unicorn_rails -c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+    end
+  end
+  desc 'Завершение Unicorn'
+  task :stop do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -9 `cat #{fetch(:unicorn_pid)}`; fi"
+    end
+  end
+end
+
+namespace :deploy do
+  after :finishing, 'application:stop'
+  after :finishing, 'application:start'
+  after :finishing, :cleanup
+end
