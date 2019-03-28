9222b2577d14b9ceedc8c2d02040b49d9dce77c9
mihail <varstt@ya.ru>
2019-03-28 22:01:21 +0500

    - User profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-    @projects = Project.find(params[:id])
+    @projects = @user.projects
   end
 end
